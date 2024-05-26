ff082af6a81d86ce3893aafde97b95e3adc5e91b
zik <a2zzuhaib@gmail.com>
2024-05-27 01:29:19 +0300

    no message

--- react_native/src/Strings.tsx
+++
@@ -1,5 +0,0 @@
-export const Strings = {
-    sdk: 'SDK',
-    version: 'v0.1.2',
-    message: "You\'re on your way to enabling your Android App to allow your customers to pay in a very easy and simple way just click the payment button and tap your payment card on NFC enabled Android phone."
-  };
