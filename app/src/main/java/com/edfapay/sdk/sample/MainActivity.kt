42725c24c8086308ebbd5c1da6fdac22a644e2d8
zik <a2zzuhaib@gmail.com>
2024-07-24 18:56:46 +0300

    Update sample for enable user to scan authcode and  enter amount

--- app/src/main/java/com/edfapay/sdk/sample/MainActivity.kt
+++ app/src/main/java/com/edfapay/sdk/sample/MainActivity.kt
@@ -1,68 +1,133 @@
 package com.edfapay.sdk.sample
 
-import android.content.Intent
+import android.R.attr.name
+import android.content.SharedPreferences
 import android.os.Bundle
-import android.widget.Button
+import android.util.Log
+import android.view.View
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.edfapay.paymentcard.EdfaPayPlugin
-import com.edfapay.paymentcard.Env
 import com.edfapay.paymentcard.model.TransactionType
 import com.edfapay.paymentcard.model.TxnParams
-import com.edfapay.paymentcard.ui.TestApisActivity
-import com.edfapay.paymentcard.ui.TestCardPaymentActivity
-import com.edfapay.paymentcard.ui.ValidateConfigsActivity
+import com.edfapay.paymentcard.model.enums.Env
+import com.edfapay.paymentcard.utils.isMocked
+import com.journeyapps.barcodescanner.ScanContract
+import com.journeyapps.barcodescanner.ScanIntentResult
+import com.journeyapps.barcodescanner.ScanOptions
+import com.journeyapps.barcodescanner.ScanOptions.QR_CODE
+
+
 class MainActivity : AppCompatActivity() {
-    val amount = "400.010"
+    lateinit var cache:SharedPreferences
+
+    // Register the launcher and result handler
+    private val barcodeLauncher = registerForActivityResult<ScanOptions, ScanIntentResult>(
+        ScanContract()
+    ) { result: ScanIntentResult ->
+        if (result.contents != null){
+            findViewById<TextView>(R.id.txtAuthCode).text = result.contents
+            val myEdit = cache.edit()
+            myEdit.putString("auth", result.contents)
+            myEdit.commit()
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        initEdfaPay()
+        cache = getSharedPreferences("EdfaPaySoftPOS", MODE_PRIVATE)
+        initView()
 
-        with(findViewById<Button>(R.id.btnTestApis)) {
+        val have = EdfaPayPlugin.haveLocationPermission(this)
+        if(!have){
+            EdfaPayPlugin.requestLocationPermission(this){
+                EdfaPayPlugin.currentLocation(this){ location, permission ->
+                    Log.i("EdfaPayPlugin.currentLocation", "Mocked: ${location?.isMocked()}")
+                    Log.i("EdfaPayPlugin.currentLocation",  it.toString())
+                }
+            }
+        }
+    }
 
-        }
-        findViewById<Button>(R.id.btnTestApis).setOnClickListener {
-            startActivity(Intent(this, TestApisActivity::class.java))
+    fun initView(){
+
+        showInitializeSdk()
+
+        findViewById<TextView>(R.id.txtAuthCode).text = cache.getString("auth", "");
+
+        findViewById<View>(R.id.btnInitialize).setOnClickListener {
+            initEdfaPay()
         }
 
-        findViewById<Button>(R.id.btnTestCardPayment).setOnClickListener {
-            startActivity(Intent(this,TestCardPaymentActivity::class.java))
-           //pay(EdfaPayPlugin)
+        findViewById<View>(R.id.btnPay).setOnClickListener {
+            this.pay()
         }
 
-        findViewById<Button>(R.id.btnValidateConfigs).setOnClickListener {
-            startActivity(Intent(this, ValidateConfigsActivity::class.java))
+        findViewById<View>(R.id.btnScanQr).setOnClickListener {
+            barcodeLauncher.launch(
+                ScanOptions()
+                    .setBarcodeImageEnabled(false)
+                    .setOrientationLocked(true)
+                    .setDesiredBarcodeFormats(QR_CODE)
+            )
         }
+    }
+
+    fun showInitializeSdk(){
+        findViewById<View>(R.id.viewInit).visibility = View.VISIBLE
+        findViewById<View>(R.id.viewPay).visibility = View.GONE
+    }
+
+    fun showPay(){
+        findViewById<View>(R.id.viewInit).visibility = View.GONE
+        findViewById<View>(R.id.viewPay).visibility = View.VISIBLE
     }
 
     fun initEdfaPay(){
 
+        val authCode = findViewById<TextView>(R.id.txtAuthCode).text.toString()
+        if(authCode.isEmpty()){
+            Toast.makeText(this, "Invalid Auth Code", Toast.LENGTH_SHORT).show()
+            return
+        }
+
         EdfaPayPlugin.initiate(
             context = this,
-            environment = Env.DEVELOPMENT,
-            authCode = "YUBmbG91Y2kuY29tOjEyMzRAV2VwYXk=", // we-settle
+            environment = Env.DEVELOPMENT,//.with("https://testdeployment.edfapay.com/"),
+            authCode = authCode, // we-settle
             onSuccess = { plugin ->
+                showPay()
+
+                EdfaPayPlugin.theme
+                    .setButtonBackgroundColor("#06E59F")
+                    .setButtonTextColor("#000000")
+                    .setHeaderImage(this, R.drawable.edfapay_text_logo)
+                    .setPoweredByImage(this, R.drawable.edfapay_text_logo)
+
+                Toast.makeText(this, "SDK Initialized Successfully", Toast.LENGTH_SHORT).show()
             }
         ){ err ->
             err.printStackTrace()
+            Toast.makeText(this, "Error Initializing: ${err.message}", Toast.LENGTH_SHORT).show()
         }
-
-        EdfaPayPlugin.theme
-            .setButtonBackgroundColor("#06E59F")
-            .setButtonTextColor("#000000")
-            .setHeaderImage(this, R.drawable.edfapay_text_logo)
-            .setPoweredByImage(this, R.drawable.edfapay_text_logo)
     }
 
-    fun pay(plugin:EdfaPayPlugin){
+    fun pay(){
+        val amount = findViewById<TextView>(R.id.txtAmount).text
+        if(amount.toString().toDoubleOrNull() == null){
+            Toast.makeText(this, "Invalid Amount", Toast.LENGTH_SHORT).show()
+            return
+        }
+
         val params = TxnParams(
-            amount = amount,
+            amount = amount.toString(),
             transactionType = TransactionType.PURCHASE,
         )
 
-        plugin.pay(
+        EdfaPayPlugin.pay(
             this,
             params,
             onRequestTimerEnd = {
@@ -94,4 +159,6 @@ class MainActivity : AppCompatActivity() {
             },
         )
     }
+
+    // Launc
 }
