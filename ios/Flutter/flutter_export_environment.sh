d1d8894db98645e4b8765c11264cee98eb0c451e
zik <a2zzuhaib@gmail.com>
2025-01-08 13:36:56 +0300

    no message

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Volumes/Data/SDK/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/zik/Desktop/untitled"
+export "FLUTTER_APPLICATION_PATH=/Volumes/Data/Projects/Edfapay/Examples/edfapay-softpos-sdk-examples"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
